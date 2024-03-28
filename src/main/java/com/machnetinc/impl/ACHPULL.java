54a2097728028b0659a4904816e3080385b3c9b5
shankar <coder.shankar@gmail.com>
2024-03-28 19:32:36 +0545

    fix conflict

---
+++ src/main/java/com/machnetinc/impl/ACHPULL.java
@@ -0,0 +1,27 @@
+package com.machnetinc.impl;
+
+
+import com.machnetinc.annotation.processor.CSVToObject;
+import com.machnetinc.annotation.processor.TransactionRequest;
+
+import java.util.List;
+
+@CSVToObject("a.csv")
+public class ACHPULL extends A {
+
+    @Override
+    public List<JournalDto> create(TransactionRequest request){
+        final List<JournalDto> journalDtos = super.create(request);
+        //add extra logic here
+
+        return journalDtos;
+    }
+
+    public void mapping(JournalDto journalDto){
+        //convert journal dto to entity
+    }
+
+
+
+
+}
